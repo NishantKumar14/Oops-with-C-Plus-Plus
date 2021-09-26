19b179c30e41f25756080de2594585d1453296c5
Nishant Kumar <nk534031@gmail.com>
2021-09-26 16:10:50 +0530

    My First Program

---
+++ Practical-Tasl-1/C++ Files/Practical_Task.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long int
+
+int main() {
+	cout << "This is my First Program!" << endl;
+	cout << "Size of long long int: " << sizeof(ll) << endl;
+	return 0;
+}
